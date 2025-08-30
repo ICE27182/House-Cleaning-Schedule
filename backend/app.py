1e3ac7c33baf74762e55ef23989fd0bb39b2ccf6
ICE27182_Laptop <ICE271828@outlook.com>
2025-08-30 21:00:49 +0200

    Added route for index page

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from backend.apis import api
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend/dist", static_url_path="")
 
 app.register_blueprint(api)
+@app.route("/")
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def index_page(path):
+    return send_from_directory(app.static_folder, "index.html")
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from backend.app import app
 from backend.db import reset
 print(str(app.url_map._rules_by_endpoint).replace("],", "],\n"))
 reset()
-app.run(host="0.0.0.0", debug=False, port=8088)
+app.run(host="0.0.0.0", debug=False, port=80)
