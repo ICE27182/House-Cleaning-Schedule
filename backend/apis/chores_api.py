b9d62aa73f45b760b4fdf3f4aa3bef4b88125763
ICE27182 <ice27182@ICE27182s-MacBook-Air.local>
2025-08-30 13:14:02 +0200

    Refactored chores

--- backend/apis/chores_api.py
+++ backend/apis/chores_api.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, jsonify
 from backend.models import chores
+from backend.db import connect_r
 
 bp = Blueprint("chores_api", __name__)
 
 @bp.route("/chores", methods=["GET"])
 def get_chores():
-    return jsonify(chores.get_all_chores())
+    with connect_r() as conn_r:
+        return jsonify(chores.get_all_chores(conn_r))
--- backend/models/chores.py
+++ backend/models/chores.py
@@ -1,5 +1,5 @@
-from backend.db import connect_r
 from typing import TypedDict
+from duckdb import DuckDBPyConnection
 
 class Chore(TypedDict):
     id: int
@@ -9,12 +9,11 @@ class Chore(TypedDict):
     people_group: str
     assignee_count: int
 
-def get_all_chores() -> list[Chore]:
-    with connect_r() as conn:
-        rows = conn.execute("SELECT id, name, description, frequency, people_group, assignee_count FROM chores").fetchall()
-        return [Chore(id=row[0],
-                      name=row[1],
-                      description=row[2],
-                      frequency=row[3],
-                      people_group=row[4],
-                      assignee_count=row[5]) for row in rows]
+def get_all_chores(conn: DuckDBPyConnection) -> list[Chore]:
+    rows = conn.execute("SELECT id, name, description, frequency, people_group, assignee_count FROM chores").fetchall()
+    return [Chore(id=row[0],
+                    name=row[1],
+                    description=row[2],
+                    frequency=row[3],
+                    people_group=row[4],
+                    assignee_count=row[5]) for row in rows]
