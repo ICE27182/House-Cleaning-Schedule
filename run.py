dcfc6eac053ae7d1d78e75b5be815a5282711304
ICE27182 <ice27182@ICE27182s-MacBook-Air.local>
2025-08-30 19:31:08 +0200

    Set the title; Removed old files; Added requirements.txt

--- run.py
+++ run.py
@@ -2,26 +2,6 @@
 
 from backend.app import app
 from backend.db import reset
-reset()
 print(str(app.url_map._rules_by_endpoint).replace("],", "],\n"))
+reset()
 app.run(host="0.0.0.0", debug=False, port=8088)
-
-# from app import create_website, app, WeekYear, Record
-
-# # app.compress_everything()
-
-# website = create_website()
-
-# from flask import redirect
-# @website.route("/")
-# def root_redirect():
-#     weekyear = app.get_weekyear()
-#     return redirect(f"/schedules/weekyear/{weekyear.week}-{weekyear.year}")
-
-
-# # I shouldn't have used run for the final product tho
-# # But it is just a small website, so it's probably fine
-# website.run("0.0.0.0", 80)
-# # website.run("::", 80)
-
-
